179c7f16d7e6d00f6810638630f4e7cd4b26127d
ahmyi <cryptoamity@gmail.com>
2022-03-25 10:06:17 +0800

    modify to use src

--- tests/modules/pool/miner.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-require('module-alias/register');
-
-global.log = function (a, b, c) {
-
-};
-const test = require('ava');
-const Miner = require('@scalapool/modules/pool/miner');
-const CoinManager = require('@scalapool/core/coin_manager');
