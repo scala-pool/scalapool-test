7e7fc7c59f876ab37555f3a42b6adc92ac13b31b
ahmyi <cryptoamity@gmail.com>
2022-04-16 01:58:11 +0800

    Update add job test

---
+++ src/modules/pool/job.js
@@ -0,0 +1,20 @@
+'use strict';
+require('module-alias/register');
+const category = __filename.replace(process.cwd() + '/tests', '');
+
+test.skip(category + ' : addJob', t => {
+	
+});
+
+test.skip(category + ' : getJobById', t => {
+	
+});
+
+test.skip(category + ' : cached', t => {
+	
+});
+
+test.skip(category + ' : fetch', t => {
+	
+});
+
