92fb1c82d5a84d2ad1b29121195c7fe7a226a637
ahmyi <cryptoamity@gmail.com>
2022-04-16 19:21:11 +0800

    Update tests

---
+++ src/model/pool_miners.js
@@ -0,0 +1,25 @@
+'use strict';
+require('module-alias/register');
+const category = __filename.replace(process.cwd() + '/tests', '');
+const test = require('ava');
+const adddress = require('@scalapool/model/pool_miners');
+
+test.skip(category + ' : clearTotalMiners', t => {
+
+});
+
+test.skip(category + ' : onConnect', t => {
+
+});
+
+test.skip(category + ' : onDisconnect', t => {
+
+});
+
+test.skip(category + ' : onSwitch', t => {
+
+});
+
+test.skip(category + ' : getStats', t => {
+
+});
